88d8546645f26b26722de2ace0d2cdc66d267d40
VictoriscominG <123qwur@mail.ru>
2026-05-01 20:58:50 +0300

    import text color and modified hello.go

--- greeting/hello.go
+++ greeting/hello.go
@@ -1,6 +1,10 @@
 package greeting
 
-var greeting string = "Golang for Brave!"
+import "github.com/fatih/color"
+
+var greeting string = (color.CyanString("Golang ")
+						+ color.RedString("for")
+						+ color.MagentaString(" Brave!"))
 
 func Hello() string {
 	return greeting
